ad37e3b8d5cd7cd480d49e3868e9152017d59c18
bunkyu3 <pq9f07ebpr@gmail.com>
2019-02-09 21:41:50 +0900

    switch EL2 to EL1 and display current level

--- 01/bootload/main.c
+++ 01/bootload/main.c
@@ -1,9 +1,14 @@
 #include "serial.h"
 #include "lib.h"
+#include "utils.h"
 
 int main(void){
 	serial_init();
 
+	char p;
+	p = (char)get_el();
+	puts("Exception level is ");
+	putc(p+0x30); puts("\n");
 	puts("Hello World!\n");
 
 	while(1)
